c3f7ea7959d21fd0f85a741a5872bb6d72b3315d
jinhyenkim01 <jinhyenkim01@gmail.com>
2025-11-03 15:56:29 +0900

    Implement price input
    The validation code that checks whether the input
    is an positive integer divisible by the price of
    one lotto ticket is also placed.
    
    Special reformat code that deletes whitespaces
    and commas are also added so that users can type
    in numbers with commas for ease of use.
    
    Also created new file "LottoConstants.java" that
    stores the "global constants" of the project.
    Currently only stores the cost of one ticket.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,62 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
+    static void divisibleByPrice(int total) {
+        if (total % LottoConstants.LOTTO_PRICE != 0) {
+            System.out.println("[ERROR] 금액은 티캣 값으로 나눌 수 있어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 금액은 티캣 값으로 나눌 수 있어야 합니다.");
+        }
+    }
+
+    static int testParseInt(String intString) {
+        try {
+            return Integer.parseInt(intString);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 입력된 값이 정수가 아닙니다.");
+            throw new IllegalArgumentException("[ERROR] 입력된 값이 정수가 아닙니다.");
+        }
+    }
+
+    static String reformat(String input) {
+        return input.replaceAll("[\\s,]+", "");
+    }
+
+    static void testPositive(int total) {
+        if (total <= 0) {
+            System.out.println("[ERROR] 금액은 0 이상이어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 금액은 0 이상이어야 합니다.");
+        }
+    }
+
+    static String askTotal() {
+        System.out.println("금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    static int validateTotal(String total) {
+        while (true) {
+            try {
+                int totalInt = testParseInt(reformat(total));
+                divisibleByPrice(totalInt);
+                testPositive(totalInt);
+                return totalInt;
+            } catch (IllegalArgumentException e) {
+                total = askTotal();
+            }
+        }
+    }
+
+    static int getTotal() {
+        String totalInput = askTotal();
+        return validateTotal(totalInput);
+    }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int total = getTotal();
+        System.out.println(total);
     }
 }
---
+++ src/main/java/lotto/LottoConstants.java
@@ -0,0 +1,5 @@
+package lotto;
+
+public class LottoConstants {
+    public static final int LOTTO_PRICE = 1000;
+}
